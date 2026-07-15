284ea7df0bcc2cd890a19f01c8deda81379864f3
Oleksandr Kiris <okiris@ad.pposiechnice.pl>
2026-07-15 12:04:00 +0200

    Improve mobile visual polish

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 ﻿const CACHE_PREFIX = "citronex-zgorzelec-bogatynia-szkolenie-";
-const CACHE_NAME = CACHE_PREFIX + "20260715-zb-location-systems1";
+const CACHE_NAME = CACHE_PREFIX + "20260715-zb-visual-polish1";
 
 const CORE_ASSETS = [
   "./",
@@ -18,10 +18,10 @@ const CORE_ASSETS = [
   "./zakazy.html",
   "./test.html",
   "./manifest.webmanifest",
-  "./assets/css/training.css?v=20260715-zb-location-systems1",
-  "./assets/js/training-data.js?v=20260715-zb-location-systems1",
-  "./assets/js/location-custom.js?v=20260715-zb-location-systems1",
-  "./assets/js/training-app.js?v=20260715-zb-location-systems1",
+  "./assets/css/training.css?v=20260715-zb-visual-polish1",
+  "./assets/js/training-data.js?v=20260715-zb-visual-polish1",
+  "./assets/js/location-custom.js?v=20260715-zb-visual-polish1",
+  "./assets/js/training-app.js?v=20260715-zb-visual-polish1",
   "./assets/brand/polskie-pomidory-logo.png",
   "./assets/brand/polskie-pomidory-icon.png",
   "./assets/orientation/sklarnia-etap-excel.png"
